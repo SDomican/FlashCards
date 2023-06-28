189b2d867836eb2de233c9c2b675b35f8ceec8c1
Domo230 <stephen.p.domican@gmail.com>
2023-06-28 08:46:51 +0100

    Added initial DTO

---
+++ FlashCards/StackDTO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlashCards
+{
+    internal class StackDTO
+    {
+        public string Name { get; set; } = string.Empty;
+        public string value { get; set; } = string.Empty;
+        public List<FlashCardDTO> CardList { get; set; } = new List<FlashCardDTO>();
+    }
+
+    internal class FlashCardDTO
+    {
+        public string Name { get; set; } = string.Empty;
+        public string value { get; set; } = string.Empty;
+    }
+}
